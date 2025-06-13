fe52370d6dbe40e996c16eb42cd645d432e88129
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-06-13 08:47:03 -0300

    Carregando os municipios de acordo com a UF selecionada

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -7,7 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -59,6 +59,10 @@ export class CadastroComponent implements OnInit {
         if (clienteEncontrado){
           this.atualizando = true;
           this.cliente = clienteEncontrado;
+          if(this.cliente.uf) {
+            const event = { value: this.cliente.uf };
+            this.carregarMunicipios(event as MatSelectChange);
+          }
         }
       }
 
@@ -72,6 +76,14 @@ export class CadastroComponent implements OnInit {
       error: erro => console.log('ocorreu um erro: ', erro)
     });
     console.log('passou por aqui');
+  }
+
+  carregarMunicipios(event: MatSelectChange) {
+    const ufSelecionada = event.value;
+    this.brasilApiService.listarMunicipios(ufSelecionada).subscribe({
+      next: listaMunicipios => this.municipios = listaMunicipios,
+      error: erro => console.log('ocorreu um erro: ', erro)
+    });
   }
 
   salvar() {
